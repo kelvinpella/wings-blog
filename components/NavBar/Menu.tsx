ffcca8e6ec3075236ff8d8fda0d4fa4f0dcd9d69
Kelvin <kelvinpella@gmail.com>
2023-05-12 18:11:50 +0300

    WB-3 - added animation to menu

--- components/NavBar/Menu.tsx
+++ components/NavBar/Menu.tsx
@@ -1,22 +1,71 @@
 import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 import NavItems from "./NavItems";
+import {
+  useTransition,
+  useSpringRef,
+  animated,
+  useChain,
+} from "@react-spring/web";
 type Props = {
   isOpen: Boolean;
   toggleMenu: () => void;
 };
+
 export default function Menu({ isOpen, toggleMenu }: Props) {
+  // backdrop ref and content ref
+  const backdropRef = useSpringRef();
+  const contentRef = useSpringRef();
+
+  //fade in / out backdrop
+  const fadeInOutBackdrop = useTransition(isOpen, {
+    ref: backdropRef,
+    from: { opacity: 0 },
+    enter: { opacity: 1 },
+    leave: { opacity: 0 },
+  });
+  // animate menu on enter & leave
+  const contentTransitions = useTransition(isOpen, {
+    ref: contentRef,
+    from: { opacity: 0, transform: "translateX(100%)" },
+    enter: { opacity: 1, transform: "translateX(0)" },
+    leave: { opacity: 0, transform: "translateX(100%)" },
+  });
+  // chain the animations
+  useChain(
+    isOpen ? [backdropRef, contentRef] : [contentRef, backdropRef],
+    [0, 0.1]
+  );
   // content shown when Menu is open
   const contentMenuOpened = (
     <div className="md:hidden w-full h-screen absolute inset-0  ">
-      <div onClick={toggleMenu} className="w-full h-full bg-black/50" />
-      <div className="absolute h-full top-0 right-0 w-8/12 bg-ghost-white pt-6 px-3 z-10 text-end">
-        <button onClick={toggleMenu}>
-          <AiOutlineClose className="text-2xl" />
-        </button>
-        <NavItems />
-      </div>
+      {fadeInOutBackdrop(
+        (style, isOpen) =>
+          isOpen && (
+            <animated.div
+              style={style}
+              onClick={toggleMenu}
+              className="w-full h-full bg-black/50"
+            />
+          )
+      )}
+
+      {contentTransitions(
+        (style, isOpen) =>
+          isOpen && (
+            <animated.div
+              style={style}
+              className="absolute h-full top-0 right-0 w-8/12 bg-ghost-white pt-6 px-3 z-10 text-end"
+            >
+              <button onClick={toggleMenu}>
+                <AiOutlineClose className="text-2xl" />
+              </button>
+              <NavItems />
+            </animated.div>
+          )
+      )}
     </div>
   );
+
   // content shown when Menu is closed
   const contentMenuClosed = (
     <button className="md:hidden" onClick={toggleMenu}>
@@ -24,5 +73,10 @@ export default function Menu({ isOpen, toggleMenu }: Props) {
     </button>
   );
 
-  return isOpen ? contentMenuOpened : contentMenuClosed;
+  return (
+    <div className="flex items-end">
+      {contentMenuClosed}
+      {isOpen && contentMenuOpened}
+    </div>
+  );
 }

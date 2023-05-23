e4983b4b272cb57cff72c77f40ae662e75d7297f
Kelvin <kelvinpella@gmail.com>
2023-05-23 19:25:04 +0300

    update postcard image

--- components/Posts/PostCard.tsx
+++ components/Posts/PostCard.tsx
@@ -16,8 +16,8 @@ export default function PostCard({ post }: { post: Post }) {
       href={`/post/${slug}`}
       className="card md:p-4 cursor-pointer hover:bg-slate-100"
     >
-      <div className="relative w-full rounded-md h-52 mb-2 md:mb-4">
-        <Image src={url} alt={alt} fill />
+      <div className="relative overflow-hidden w-full rounded-md h-52 mb-2 md:mb-4">
+        <Image src={url} alt={alt} fill className='object-cover'/>
       </div>
       <h1 className="md:my-4">{title}</h1>
       <Author src={profileImage} name={name} date={_createdAt} />
